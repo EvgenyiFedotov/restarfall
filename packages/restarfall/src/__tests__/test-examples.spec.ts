dd839eccae117c09a19930860054b2f5b2575988
Evgeniy Fedotov <fedotov1290@gmail.com>
2023-09-13 16:29:28 +0300

    test: add test-examples

---
+++ packages/restarfall/src/__tests__/test-examples.spec.ts
@@ -0,0 +1,54 @@
+import { create, use } from "../index";
+
+let shape = create.shape();
+
+beforeEach(() => (shape = create.shape()));
+
+test("check value of store", () => {
+  const $count = create.store<number>(0);
+
+  const counter = create.component(() => {
+    const setCount = use.dispatch($count);
+    setCount(7);
+    return null;
+  });
+
+  shape.attach(counter());
+
+  expect(shape.getValue($count)).toBe(7);
+});
+
+test("check state of event", () => {
+  const changeCount = create.event<number>();
+
+  const counter = create.component(() => {
+    const change = use.dispatch(changeCount);
+    change(2);
+    return null;
+  });
+
+  shape.attach(counter());
+
+  expect(shape.getEventState(changeCount)).toEqual({ payload: 2 });
+});
+
+test("async check value of store", async () => {
+  const request = () => {
+    return new Promise<number>((resolve) => setTimeout(() => resolve(3), 200));
+  };
+
+  const $count = create.store<number>(0);
+
+  const counter = create.component(() => {
+    const setCount = use.dispatch($count);
+
+    use.promise(request()).then(setCount);
+
+    return null;
+  });
+
+  shape.attach(counter());
+  await shape.wait();
+
+  expect(shape.getValue($count)).toBe(3);
+});
