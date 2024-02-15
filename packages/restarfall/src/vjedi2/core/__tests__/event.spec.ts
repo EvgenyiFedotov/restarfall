6b68c81426c14f316d2cd7fd33c1d0da7acf1c93
Evgeniy Fedotov <fedotov1290@gmail.com>
2024-02-15 12:25:58 +0300

    chore: remove prev versions of new approach

--- packages/restarfall/src/vjedi2/core/__tests__/event.spec.ts
+++
@@ -1,9 +0,0 @@
-import { createEvent } from "../event";
-
-describe("createEvent", () => {
-  test("instance", () => {
-    const event = createEvent();
-
-    expect(event).toEqual({ type: "event" });
-  });
-});
--- packages/restarfall/src/vjedi2/core/event.ts
+++
@@ -1,9 +0,0 @@
-interface Event<Payload> {
-  readonly type: "event";
-  readonly value?: Payload; // Need for resolve types
-}
-
-const createEvent = <Payload>(): Event<Payload> => ({ type: "event" });
-
-export type { Event };
-export { createEvent };
